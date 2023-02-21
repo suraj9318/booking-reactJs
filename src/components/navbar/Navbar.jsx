1da6280aee9ef923d2886c15e72d4736bca20a58
suraj <surajmishra9318@gmail.com>
2023-02-21 21:15:04 +0530

    modification in navbar fearuted property done with api

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import "./navbar.css"
-
+import {Link} from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
+        <Link to='/'>
+          <span className="logo">surajBooking.com</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
