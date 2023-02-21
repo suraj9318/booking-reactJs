b6b8d6fda83fdba8b51eddaedb55cdeda29b5fff
suraj <surajmishra9318@gmail.com>
2023-02-21 20:37:55 +0530

    property list api and ui done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,7 +3,6 @@ import "./featured.css";
 
 const Featured = () => {
  const  {data,loading,error} = useFetch("/hotels/countBycityName?cities=berlin,london,madrid")
- console.log(data);
   return (
     <div className="featured">
       {loading ? "Loading please wait ..." :<>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,42 +1,37 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios'; 
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const useFetch = (url) => {
-    const [data,setData] = useState([])
-    const [error,setError] = useState(false)
-    const [loading,setLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    useEffect(()=>{
-        const fetchData = async()=>{
-            setLoading(true);
-            try {
-                const response =await axios.get(url)
-                setData(response.data);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
 
-            } catch (error) {
-                setError(true);
-            }
-            setLoading(false);
-        }
-
-        fetchData()
-    },[url])
-
-
-    const reFetch = async()=>{
-        setLoading(true);
-        try {
-            const response =await axios.get(url)
-            setData(response.data);
-
-        } catch (error) {
-            setError(true);
-        }
-        setLoading(false);
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
     }
+    setLoading(false);
+  };
 
-    return {data,loading,error,reFetch}
+  return { data, loading, error, reFetch };
+};
 
-}
-
-export default useFetch
+export default useFetch;
